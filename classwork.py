aa79824a540ea2e4177db9b436453c20889b25e9
Rigzin Dorji <02230074.cst@rub.edu.bt>
2024-02-29 20:48:16 +0600

    try

---
+++ classwork.py
@@ -0,0 +1,14 @@
+name = input("enter your name: ")
+# prompt the user to enter two values
+num1 = float(input("enter any first number: "))
+num2 = float(input("enter any second number: "))
+addition_result = num1 + num2
+print(f"addition_result = {addition_result}")
+subtraction_result = num1 - num2
+print(f"subtraction_result = {subtraction_result}")
+multiplication_result = num1 * num2
+print(f"multiplication_result = {multiplication_result}")
+division_result = num1 / num2
+print(f"division-result = {division_result}")
+
+print("You can do it, DONT GIVE UP!!")
