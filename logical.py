36310d555e519a94397bd4f709d1453705fe2e6f
Rigzin Dorji <02230074.cst@rub.edu.bt>
2024-03-01 15:04:43 +0600

    operator

---
+++ logical.py
@@ -0,0 +1,23 @@
+# AND operaator
+# example1
+x = 5
+print(x > 3 and x < 10)
+# example2
+y = 12
+print(y > 10 and y % 5 == 0 )
+
+# OR operator
+# example1
+x = 5
+print(x < 3 or x > 10)
+# example2
+y = 12
+print(y < 10 or y % 2 == 0 )
+
+# NOT operator
+# example1
+x = 5
+print(not(x > 3 and x < 10))
+# example2
+y = 12
+print(not(y > 10 and y % 5 == 0))
